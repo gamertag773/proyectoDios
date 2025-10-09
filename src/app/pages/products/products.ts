1d5ea49aa1fca36515ebc8199799e1f6a1ff44f5
gamertag773 <brunocarracarrasco@gmail.com>
2025-10-09 16:49:40 -0300

    clase 2-diajueves9

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -19,36 +19,42 @@ export class Products {
     this.infotarjeta = [
       {
         id: "1a",
-        img:"../../../assets/iphone13promax.jpg",
-        alt:"iphone13promax",
+        img: "../../../assets/iphone13promax.jpg",
+        alt: "iphone13promax",
         titulo: "iphone 13 ProMax",
         descripcion: "iphone 13 pro-max a la venta",
         anualpublication: 2021
       },
-      { 
+      {
         id: "1b",
-        img:"../../../assets/iphone14promax.jpg",
-        alt:"iphone14promax",
+        img: "../../../assets/iphone14promax.jpg",
+        alt: "iphone14promax",
         titulo: "iphone 14 ProMax",
         descripcion: "iphone 14 pro-max a la venta",
         anualpublication: 2022
       },
-            { 
+      {
         id: "1c",
-        img:"../../../assets/iphone15promax-azul.webp",
-        alt:"iphone15promax",
+        img: "../../../assets/iphone15promax-azul.webp",
+        alt: "iphone15promax",
         titulo: "iphone 15 ProMax",
         descripcion: "iphone 15 pro-max a la venta",
         anualpublication: 2023
       },
-            { 
+      {
         id: "1d",
-        img:"../../../assets/jsjsjsj.webp",
-        alt:"iphone16promax",
+        img: "../../../assets/jsjsjsj.webp",
+        alt: "iphone16promax",
         titulo: "iphone 16 ProMax",
         descripcion: "iphone 16 pro-max a la venta",
         anualpublication: 2024
       },
     ]
   }
+
+  tarjetaseleccionada: any = null;
+  vermas(tarjeta: any) {
+    this.tarjetaseleccionada = tarjeta;
+  }
+
 }
