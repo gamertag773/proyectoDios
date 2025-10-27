f12b7441843e824d8bed3065e004ca337a578a08
gamertag773 <brunocarracarrasco@gmail.com>
2025-10-27 18:49:25 -0300

    dialunes25

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -19,35 +19,67 @@ export class Products {
     this.infotarjeta = [
       {
         id: "1a",
-        img: "../../../assets/iphone13promax.jpg",
-        alt: "iphone13promax",
-        titulo: "iphone 13 ProMax",
-        descripcion: "iphone 13 pro-max a la venta",
-        anualpublication: 2021
+        img: "../../../assets/iphoneairspaceblack.webp",
+        alt: "iphone Air",
+        titulo: "iphone Air",
+        descripcion: "iphone Air a la venta",
+        anualpublication: 2025
       },
       {
         id: "1b",
-        img: "../../../assets/iphone14promax.jpg",
-        alt: "iphone14promax",
-        titulo: "iphone 14 ProMax",
-        descripcion: "iphone 14 pro-max a la venta",
-        anualpublication: 2022
+        img: "../../../assets/iphone17white.webp",
+        alt: "iphone17",
+        titulo: "iphone 17",
+        descripcion: "iphone 17 a la venta",
+        anualpublication: 2025
       },
       {
         id: "1c",
-        img: "../../../assets/iphone15promax-azul.webp",
-        alt: "iphone15promax",
-        titulo: "iphone 15 ProMax",
-        descripcion: "iphone 15 pro-max a la venta",
-        anualpublication: 2023
+        img: "../../../assets/iphone17prodeepblue.webp",
+        alt: "iphone17pro",
+        titulo: "iphone 17 Pro",
+        descripcion: "iphone 17 pro a la venta",
+        anualpublication: 2025
       },
       {
         id: "1d",
-        img: "../../../assets/jsjsjsj.webp",
-        alt: "iphone16promax",
-        titulo: "iphone 16 ProMax",
-        descripcion: "iphone 16 pro-max a la venta",
-        anualpublication: 2024
+        img: "../../../assets/iphone16eblack.webp",
+        alt: "iphone16e",
+        titulo: "iphone 16e",
+        descripcion: "iphone 16e a la venta",
+        anualpublication: 2025
+      },
+      {
+        id: "2a",
+        img: "../../../assets/airpodspro3.jpg",
+        alt: "airpods pro3",
+        titulo: "airpods pro3",
+        descripcion: "airpods pro3 a la venta",
+        anualpublication: 2025
+      },
+      {
+        id: "2b",
+        img: "../../../assets/ipadprofinish.webp",
+        alt: "ipadpro",
+        titulo: "ipad pro",
+        descripcion: "ipad pro a la venta",
+        anualpublication: 2025
+      },
+      {
+        id: "2c",
+        img: "../../../assets/macbookpro11.png",
+        alt: "macbook pro",
+        titulo: "macbook pro",
+        descripcion: "macbook pro a la venta",
+        anualpublication: 2025
+      },
+      {
+        id: "2d",
+        img: "../../../assets/s11case.jpg",
+        alt: "apple watch",
+        titulo: "apple watch s11",
+        descripcion: "apple watch s11 a la venta",
+        anualpublication: 2025
       },
     ]
   }
